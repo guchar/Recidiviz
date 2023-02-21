2fd8f696621dab798918ef6f75995da23be85a7a
Charlie <charliegu@berkeley.edu>
2023-02-21 12:16:48 -0800

    final version

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -3,22 +3,45 @@ import './App.css'
 
 function App() {
 
-    const apiKey = '3608b6060d343e91ca3c9ba44141abdd'
+    const apiKey = 'afb5899925b1a3f4e22afbaa2c787f76'
 
     const [weatherData, setWeatherData] = useState([{}])
     const [city, setCity] = useState("")
 
     const getWeather = (event) => {
         if (event.key == "Enter") {
-            fetch()
+            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&APPID=${apiKey}`).then(
+                response => response.json()
+            ).then(
+                data => {
+                    setWeatherData(data)
+                    setCity("")
+                }
+            )
         }
     }
 
     return (
-        <div className= 'container' >
-            <input className= 'input' placeholder = 'Enter city...' />
-            
+        <div className= "container" >
+            <input 
+            className= "input"
+            placeholder = "Enter city..."
+            onChange = {e => setCity(e.target.value)}
+            value = {city}
+            onKeyPress = {getWeather}
+            />
 
+            {typeof weatherData.main === 'undefined' ? (
+                <div>
+                    <p> Welcome to weather app! Enter in a city to get the weather of. </p>
+                </div>
+            ) : (
+                <div className='weather-data'>
+                    <p className='city'>{weatherData.name}</p>
+                    <p className='temp'>{Math.round(weatherData.main.temp)}°F</p>
+                    <p className='weather'>{weatherData.weather[0].main}</p>
+                </div>
+            )}
         </div>
     )
 }
