612cefd169fb4ccaea4c83001ef90fd0820ff661
Maria Romano <maria.romano@berkeley.edu>
2023-02-23 22:03:54 -0800

    other weather data added. fix css

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import moment from 'moment'
 import "./App.css";
 // import { useMemo } from "react";
 // import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api"
@@ -31,12 +31,18 @@ function App() {
           setCity("");
         });
     }
-  };
+  }; 
+
+  // var sunrise = weatherData.sys.sunrise;
+  // const humanTime = new Date(sunrise * 1000).toLocaleString();
+  // var sunset = weatherData.sys.sunset;
+  // const humanTime2 = new Date(sunset * 1000).toLocaleString();
+
 
   return (
     <div className="container">
       <header>
-        <h1>☀️MarChar's Weather App☁️</h1>
+        <h1 className="title">☀️MarChar's Weather App☁️</h1>
       </header>
       <input
         className="input"
@@ -45,24 +51,36 @@ function App() {
         value={city}
         onKeyPress={getWeather}
       />
-
       {typeof weatherData.main === "undefined" ? (
         <div>
-          <p>
+          <p className="welcome-msg">
             {" "}
             Welcome to our weather app! Enter in a city to see the weather!{" "}
           </p>
         </div>
       ) : (
-        <div className="weather-data">
-          <p className="city">{weatherData.name}</p>
-          <p className="temp">{Math.round(weatherData.main.temp)}°F</p>
-          <p className="weather">{weatherData.weather[0].main}</p>
-          <p className="footnote">
-            {" "}
-            Authors: Charles Gu and Maria Romano. Inspiration from Arpan
-            Neupane.
-          </p>
+        <div className = "all-weather"> 
+          <div className="weather-data">
+            <p className="city">{weatherData.name}</p> 
+          
+            <p className="temp">{Math.round(weatherData.main.temp)}°F</p>
+            <p className="weather">{weatherData.weather[0].main}</p>
+            <p className="footnote">
+              {" "}
+              Authors: Charles Gu and Maria Romano. Inspiration from Arpan
+              Neupane.
+            </p>
+          </div>
+          <div className="other-weather-data">
+              <p className = "max"> {"Max : " + Math.round(weatherData.main.temp_max)} </p>
+              <p className = "max"> {"Min : " + Math.round(weatherData.main.temp_min)} </p>
+              <p className = "feels_like"> {"Feels like : " + Math.round(weatherData.main.feels_like)} </p>
+              <p className = "humidity"> {"Humidity : " + Math.round(weatherData.main.humidity) + "%"} </p>
+              <p className = "wind"> {"Wind Speed : " + Math.round(weatherData.wind.speed) + " mph"} </p>
+              {/* <p className = "sunset"> {"Sunrise : " + Math.round(weatherData.sys.sunrise)} </p>  */}
+              {/* <p className = "sunrise"> {"Sunrise : " + humanTime} </p> */}
+              {/* /* <p className = "sunset"> {"Sunset : " + humanTime2} </p> */}
+            </div>
         </div>
       )}
     </div>
@@ -70,3 +88,8 @@ function App() {
 }
 
 export default App;
+
+
+// var t = new Date();
+// t.setSeconds(1370001284);
+// var formatted = moment(t).format("dd.mm.yyyy hh:MM:ss");
